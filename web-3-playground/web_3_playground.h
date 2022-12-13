c9405b286f3da46412dc68ae60c79f8812478f8b
TamaraGambarova <61021836+TamaraGambarova@users.noreply.github.com>
2022-12-13 09:47:43 +0100

    Initial Commit

---
+++ web-3-playground/web_3_playground.h
@@ -0,0 +1,18 @@
+//
+//  web_3_playground.h
+//  web-3-playground
+//
+//  Created by Tamara Gambarova on 13.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for web_3_playground.
+FOUNDATION_EXPORT double web_3_playgroundVersionNumber;
+
+//! Project version string for web_3_playground.
+FOUNDATION_EXPORT const unsigned char web_3_playgroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <web_3_playground/PublicHeader.h>
+
+
