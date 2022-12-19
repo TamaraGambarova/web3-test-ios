19c9884eaaf40a498ca57d3d9a8ed3a1f15443db
TamaraGambarova <61021836+TamaraGambarova@users.noreply.github.com>
2022-12-19 14:45:27 +0100

    Fix

---
+++ web-3-playground/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  web-3-playground
+//
+//  Created by Tamara Gambarova on 13.12.2022.
+//
+
+import Foundation
---
+++ web-3-playground/TxSenderCheck.swift
@@ -0,0 +1,8 @@
+//
+//  TxSenderCheck.swift
+//  web-3-playground
+//
+//  Created by Tamara Gambarova on 15.12.2022.
+//
+
+import Foundation
